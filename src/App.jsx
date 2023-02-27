9853aee3a3c342d2ba68b87dac4d75ac4e4cf604
Muhamed Karahasanovic <muhamed.karahasanovic@sers.si>
2023-02-27 19:32:33 +0100

    add input row component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import InputRow from "./Container/InputRow/InputRow";
 
 function App() {
-  return <div>Starting point</div>;
+  return (
+    <div className="mainContainer">
+      <h1>Mohi's to-do list</h1>
+      <InputRow />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Container/InputRow/InputRow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./InputRow.css";
+
+const InputRow = () => {
+  return (
+    <div className="inputRowContainer">
+      <input type="text" />
+      <button>Add task</button>
+    </div>
+  );
+};
+
+export default InputRow;
